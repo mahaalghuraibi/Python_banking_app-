dc8d69f32e2dcc00d4688019856cbabf5668e72c
User Name <maha.alghuraibi@outlook.com>
2025-09-21 16:05:50 +0300

    update

--- banking.py
+++ banking.py
@@ -1,25 +1,44 @@
-# نستورد مكتبة CSV
-import csv  
+# مكتبة CSV
+import csv
 
-# اسم ملف البيانات (ثابت)
+# ملف البيانات
 DATA_FILE = "bank.csv"
 
 class Customer:
-    # هنا لاحقاً بنضيف خصائص (id, first_name, ...)
+    def __init__(self, first_name, last_name, password, checking=0, savings=0):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+        self.checking = checking
+        self.savings = savings
+
+    def deposit(self, amount, account_type="checking"):
+        pass
+
+    def withdraw(self, amount, account_type="checking"):
+        pass
+
+    def transfer(self, amount, from_account="checking", to_account="savings"):
+        pass
+
+    def __str__(self):
+        return f"Customer: {self.first_name} {self.last_name}, Checking: {self.checking}, Savings: {self.savings}"
+
+
+class Bank:
     pass
 
+
 class Account:
-    # الكلاس الأساسي اللي يورّث منه الحسابات
+    # كلاس أساسي للحسابات
     pass
+
 
 class CheckingAccount(Account):
     # حساب جاري
     pass
 
+
 class SavingsAccount(Account):
     # حساب توفير
-    pass
-
-class Bank:
-    # البنك نفسه (يتعامل مع العملاء ويقرأ من CSV)
     pass
